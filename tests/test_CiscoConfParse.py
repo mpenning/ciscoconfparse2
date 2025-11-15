14b9fc350d0a03199a3eb62d2dd54fbcec8d3013
mpenning <mike@pennington.net>
2025-11-15 10:36:19 -0600

    test: Rename variable

--- tests/test_CiscoConfParse.py
+++ tests/test_CiscoConfParse.py
@@ -3330,18 +3330,20 @@ def testValues_ConfigList_delete01():
     """
 
     config = "hostname foo\n!"
-    parse = CiscoConfParse(config)
+    parse_uut = CiscoConfParse(config)
 
-    assert parse.config_objs[0].text == "hostname foo"
-    assert parse.config_objs[0].linenum == 0
+    # Ensure we have 'hostname foo' as config line number 0
+    assert parse_uut.config_objs[0].text == "hostname foo"
+    assert parse_uut.config_objs[0].linenum == 0
+    assert len(parse_uut.config_objs) == 2
 
-    del parse.config_objs[0]
+    del parse_uut.config_objs[0]
 
     # Test that lines are deleted and correctly renumbered...
-    assert parse.config_objs[0].text == "!"
-    assert parse.config_objs[0].linenum == 0
-    assert len(parse.config_objs) == 1
-    assert len(parse.objs) == 1
+    assert parse_uut.config_objs[0].text == "!"
+    assert parse_uut.config_objs[0].linenum == 0
+    assert len(parse_uut.config_objs) == 1
+    assert len(parse_uut.objs) == 1
 
 
 def testValues_ConfigList_context_manager_01():
